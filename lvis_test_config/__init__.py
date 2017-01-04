0df0681253dd54be3b1ed738a08ebb9181e804f0
Jorgen Ader <jorgen@thorgate.eu>
2017-01-04 10:08:28 +0200

    fix env vars

--- lvis_test_config/__init__.py
+++ lvis_test_config/__init__.py
@@ -9,6 +9,6 @@ except ImportError:
     TEST_CERTIFICATE_PATH = os.environ.get('TEST_CERTIFICATE_PATH')
     TEST_CERTIFICATE_PERSON_CODE = os.environ.get('TEST_CERTIFICATE_PERSON_CODE')
     TEST_CERTIFICATE_PASSWORD = os.environ.get('TEST_CERTIFICATE_PASSWORD')
-    if not (PROXY_HOST_URL or TEST_CERTIFICATE_PATH or TEST_CERTIFICATE_PERSON_CODE or TEST_CERTIFICATE_PASSWORD):
+    if None in (PROXY_HOST_URL, TEST_CERTIFICATE_PATH, TEST_CERTIFICATE_PERSON_CODE, TEST_CERTIFICATE_PASSWORD):
         sys.stderr.write("Couldn't import lvis_test_config.local or ENV var not set, have you created it from lvis_test_config/local.py.example?\n")
         sys.exit(1)
